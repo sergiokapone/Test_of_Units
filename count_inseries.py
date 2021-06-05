052b6dd3d107741534354d5231e7ba9242bec63d
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-05 15:35:55 +0300

    some changes

--- count_inseries.py
+++ count_inseries.py
@@ -4,27 +4,18 @@ Created on Sun Nov 20 18:43:08 2016
 
 @author: Sergiokapone
 
-Програма розраховує кількість вказаних цифр в числах від n1 до n2 
+Функція get_num_count розраховує кількість вказаних цифр numeral в числах від n1 до n2 
 """
-
-numeral = '6'
-n1 = 1
-n2 = 176
-#lst = list(filter(lambda y: any(char == numeral for char in y), [str(x) for x in range(1,100)]))
-
-
-lst = list(
-           filter(
-                  lambda y: any(char == numeral for char in y),
-                  [item for sublist in
-                  [str(x) for x in range(n1,n2+1)]
-                  for item in sublist])
-           )
-
-print(len(lst))
-n = 0
-for num in range(len(lst)):
-    n += lst[num].count(numeral)
-
-print(f'Всього циферок {numeral} в межах від {n1} до {n2} міститься {n} штук')
-
+def get_num_count(numeral, n1, n2):
+    numeral_str = str(numeral)
+    lst = list(
+                filter(
+                    lambda y: any(char == numeral_str for char in y),
+                    [item for sublist in
+                    [str(x) for x in range(n1,n2+1)]
+                    for item in sublist])
+              )
+    n = 0
+    for num in range(len(lst)):
+        n += lst[num].count(numeral_str)
+    return n 
