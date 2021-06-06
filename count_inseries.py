37f205f0dd1002daf3d3fbd0c32d18a6d298f795
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-07 01:25:00 +0300

    Code logic revision

--- count_inseries.py
+++ count_inseries.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 20 18:43:08 2016
-
-@author: Sergiokapone
-
-Функція get_num_count розраховує кількість вказаних цифр {numeral} в числах від {n1} до {n2} 
-"""
 def get_num_count(numeral, n1, n2):
-    numeral_str = str(numeral)
-    lst = list(
-                filter(
-                    lambda y: any(char == numeral_str for char in y), # створюємо список лише з тих символів, які задовольняють умові 
-                    [item for sublist in
-                        [str(x) for x in range(n1,n2+1)]
-                    for item in sublist]    # перетворюємо всі числа в межах від {n1} до {n2} на сучільний список символів строкового типу
-                    )
-              )
-    return len(lst)
+
+    """
+    Function calculate number of specified digits {numeral} in numbers from {n1} to {n2}
+    """    
     
+    numeral_str = str(numeral)
+    
+    # Convert all numbers from {n1} to {n2} to list of strings
+    list_of_numbers = [str(x) for x in range(n1,n2+1)]  
+    
+    # Convert list of numbers to string number_string 
+    number_string = ''.join(map(str, list_of_numbers))  
+    
+    # Create list containing desired {numeral} by mask
+    list_of_desired_numbers = list(filter(lambda numbers: any(char == numeral_str for char in numbers), number_string)) 
+
+    return len(list_of_desired_numbers)
--- test.py
+++ test.py
@@ -1,15 +1,30 @@
 import unittest
 from count_inseries import get_num_count
-class NumbersCaseTest(unittest.TestCase):
-    """Тести для функції get_num_count з count_inseries.py """
+
+class GetNumCountTestCase(unittest.TestCase):
+
+    """Tests for function get_num_count from count_inseries.py """
+
     def test_5_num_in_1_100(self):
-        """Перевіряємо скільки 5-рок в межаах від 1 до 100"""
+    
+        """Перевіряємо скільки 5-рок в межаах від 1 до 100""" 
+        
         counts = get_num_count(5, 1, 100) 
         self.assertEqual(counts, 20)
-    def test_6_num_in_1_666(self):
-        """Перевіряємо скільки 6-рок в межаах від 1 до 666"""
+
+    def test_6_num_in_1_666(self):  
+    
+        """Перевіряємо скільки 6-рок в межаах від 1 до 666""" 
+        
         counts = get_num_count(6, 1, 666) 
         self.assertEqual(counts, 201)
+        
+    def test_5_num_in_50_55(self):  
+    
+        """Перевіряємо скільки 5-рок в межаах від 50 до 55""" 
+        
+        counts = get_num_count(5, 50, 55) 
+        self.assertEqual(counts, 7)
         
 if __name__== "__main__":
     unittest.main()
