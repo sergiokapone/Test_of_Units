528f70fde043a47ab38ba6d3686f37a31602e274
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-07 01:31:41 +0300

    Minor code improving

--- count_inseries.py
+++ count_inseries.py
@@ -5,12 +5,9 @@ def get_num_count(numeral, n1, n2):
     """    
     
     numeral_str = str(numeral)
-    
-    # Convert all numbers from {n1} to {n2} to list of strings
-    list_of_numbers = [str(x) for x in range(n1,n2+1)]  
-    
-    # Convert list of numbers to string number_string 
-    number_string = ''.join(map(str, list_of_numbers))  
+      
+    # Convert numbers from {n1} to {n2} to string number_string 
+    number_string = ''.join(map(str, range(n1,n2+1)))  
     
     # Create list containing desired {numeral} by mask
     list_of_desired_numbers = list(filter(lambda numbers: any(char == numeral_str for char in numbers), number_string)) 
