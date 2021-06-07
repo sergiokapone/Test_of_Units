cd5c1dc9f66e6a6e0684cd43ff34a2032b8531ce
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-07 11:09:23 +0300

    Another portion of PEP8 code adapting

--- count_inseries.py
+++ count_inseries.py
@@ -9,14 +9,13 @@ def get_num_count(numeral, n1, n2):
     numeral_str = str(numeral)
 
     # Convert numbers from {n1} to {n2} to string number_string
-    number_string = ''.join(map(str, range(n1, n2+1)))
+    number_string = "".join(map(str, range(n1, n2 + 1)))
 
     # Create list containing desired {numeral} by mask
     list_of_desired_numbers = list(
         filter(
-            lambda numbers: any(char == numeral_str for char in numbers),
-            number_string
-            )
+            lambda numbers: any(char == numeral_str for char in numbers), number_string
         )
+    )
 
     return len(list_of_desired_numbers)
