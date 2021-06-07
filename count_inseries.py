7ac3ec3bd1e4750dccf42c1ec415ce76bc7e1fd8
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-07 08:49:06 +0300

    PEP8 code adapting

--- count_inseries.py
+++ count_inseries.py
@@ -1,21 +1,22 @@
 def get_num_count(numeral, n1, n2):
+    """
 
-    """ 
-    
-    Function calculate number of specified digits {numeral} in numbers from {n1} to {n2} 
-    
-    """    
-   
+    Function calculate number of specified
+    digits {numeral} in numbers from {n1} to {n2}
+
+    """
+
     numeral_str = str(numeral)
-            
-    # Convert numbers from {n1} to {n2} to string number_string 
-    number_string = ''.join(map(str, range(n1,n2+1)))       
-    
+
+    # Convert numbers from {n1} to {n2} to string number_string
+    number_string = ''.join(map(str, range(n1, n2+1)))
+
     # Create list containing desired {numeral} by mask
     list_of_desired_numbers = list(
         filter(
-            lambda numbers: any(char == numeral_str for char in numbers), number_string
+            lambda numbers: any(char == numeral_str for char in numbers),
+            number_string
             )
         )
-     
+
     return len(list_of_desired_numbers)
--- test.py
+++ test.py
@@ -5,46 +5,41 @@ from count_inseries import get_num_count
 class GetNumCountTestCase(unittest.TestCase):
 
     """
-    
-    Tests for function get_num_count from count_inseries.py 
-    
+
+    Tests for function get_num_count from count_inseries.py
+
     """
 
     def test_5_num_in_1_100(self):
-    
         """
-        
+
         Перевіряємо скільки 5-рок в межаах від 1 до 100
-        
-        """ 
-        
-        counts = get_num_count(5, 1, 100) 
+
+        """
+
+        counts = get_num_count(5, 1, 100)
         self.assertEqual(counts, 20)
 
+    def test_6_num_in_1_666(self):
+        """
 
-    def test_6_num_in_1_666(self):  
-    
-        """
-        
         Перевіряємо скільки 6-рок в межаах від 1 до 666
-        
-        """ 
-        
-        counts = get_num_count(6, 1, 666) 
-        self.assertEqual(counts, 201)
- 
-       
-    def test_5_num_in_50_55(self):  
-    
+
         """
-        
+
+        counts = get_num_count(6, 1, 666)
+        self.assertEqual(counts, 201)
+
+    def test_5_num_in_50_55(self):
+        """
+
         Перевіряємо скільки 5-рок в межаах від 50 до 55
-        
-        """ 
-        
-        counts = get_num_count(5, 50, 55) 
+
+        """
+
+        counts = get_num_count(5, 50, 55)
         self.assertEqual(counts, 7)
- 
-       
-if __name__== "__main__":
+
+
+if __name__ == "__main__":
     unittest.main()
