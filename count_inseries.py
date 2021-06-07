1b4ce12b2e22adac856dbe87f4d7f5d6647aef07
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-07 08:21:28 +0300

    PEP8 code adapting

--- count_inseries.py
+++ count_inseries.py
@@ -1,14 +1,15 @@
 def get_num_count(numeral, n1, n2):
 
-    """
-    Function calculate number of specified digits {numeral} in numbers from {n1} to {n2}
+    """ 
+    
+    Function calculate number of specified digits {numeral} in numbers from {n1} to {n2} 
+    
     """    
-    
+   
     numeral_str = str(numeral)
-      
+            
     # Convert numbers from {n1} to {n2} to string number_string 
-    number_string = ''.join(map(str, range(n1,n2+1)))
-    
+    number_string = ''.join(map(str, range(n1,n2+1)))       
     
     # Create list containing desired {numeral} by mask
     list_of_desired_numbers = list(
