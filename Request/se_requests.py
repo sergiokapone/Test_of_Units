eeb0128c7428f4fadbe42f8ee8a9046ce8ad210e
sergiokapone <sergiy.ponomarenko@gmail.com>
2021-06-13 08:40:25 +0300

    Sync

--- Request/se_requests.py
+++ Request/se_requests.py
@@ -1,95 +1,104 @@
+"""Get API from SE sites and return uder information."""
+
 import requests
 import csv
+import json
 import re
 
 
-def request_for_questions(user_id, se_site):
-    """
+def request_for_questions(user_id, se_site, se_method):
+    """Get request for SE and return json dict."""
+    se_url = f"https://api.stackexchange.com/2.2/users/{user_id}/{se_method}"
 
-    Parameters
-    ----------
-    user_id : int
-        equal 690 for physics.se
-        equal 66024 for tex.se
-    se_site : string
-        physics for physics.se
-        tex for TeX.se
+    params = {
+        "pagesize": 100,
+        "site": f"{se_site}",
+        "sort": "activity",
+        "order": "desc",
+    }
 
-    Returns
-    -------
-    json
-        Data about user questions.
+    response = requests.get(se_url, params=params)
 
-    """
-    return (requests.get(f"https://api.stackexchange.com/2.2/users/{user_id}/questions",
-                         params={'pagesize': 100,
-                                 'site': f'{se_site}',
-                                 'sort': 'activity',
-                                 'order': 'desc'})).json()
-
-# with open("usertex.json", "r") as user_data_file:
-#     user_info_tex = json.load(user_data_file)
+    return response.json()
 
 
-def create_user_data_files(user_info, type):
-    """
+def create_user_info_data_file(info_dict, csv_filename):
+    with open(csv_filename, "w", newline="", encoding="utf-8") as csv_file:
+        # Create object responsible for converting the user’s data to .csv
+        writer = csv.writer(csv_file, delimiter=";")
+        info_dict.pop
+        # writer.writeheader()
+        for data in info_dict["items"]:
+            writer.writerow(data)
 
-    Parameters
-    ----------
-    user_info : json
-        Data about user questions.
-    type : string
-                physics for physics.se
-        tex for TeX.se
 
-    Returns
-    -------
-    None.
+def create_user_data_file(user_info_dict, type, method):
+    """Create user data .csv-file."""
+    csv_user_data_file_name = f"user_{type}_{method}.csv"
 
-    Creates
-    -------
-    .csv-file with user questions (title, link and score)
-    """
-    with open(f"user{type}.csv", "w",
-              newline="", encoding="utf-8") as csv_user_data_file:
-
+    with open(
+        csv_user_data_file_name, "w", newline="", encoding="utf-8"
+    ) as csv_user_data_file:
         # Create object responsible for converting the user’s data to .csv
         writer = csv.writer(csv_user_data_file, delimiter=";")
 
-        # Save head to .csv-file
-        writer.writerow(["title", "link", "score"])
+        se_api_methods = ["questions", "answers", "posts"]
+
+        if method == se_api_methods[0]:
+            csv_head_line = ["title", "link", "score"]
+        elif method == se_api_methods[1]:
+            csv_head_line = ["answer_id", "question_id", "score"]
+        elif method == se_api_methods[2]:
+            csv_head_line = ["link", "score"]
+
+        # Write head to .csv-file
+        writer.writerow(csv_head_line)
 
         # Write data from user_info.json to .csv
-        for i in range(len(user_info['items'])):
-            writer.writerow([user_info['items'][i]['title'],
-                             user_info['items'][i]['link'],
-                             user_info['items'][i]['score']])
+        csv_line = []
+        dict_len = len(user_info_dict["items"])
+        if dict_len:
+            for i in range(dict_len):
+                for j in csv_head_line:
+                    if j in user_info_dict["items"][i].keys():
+                        csv_line.append(user_info_dict["items"][i][j])
+                writer.writerow(csv_line)
+                csv_line.clear()
 
     # Cleaning file for LaTeX typesetting
-    clean_csv_for_LaTeX(f"user{type}.csv")
+    clean_csv_for_LaTeX(csv_user_data_file_name)
 
 
 def clean_csv_for_LaTeX(file):
-    """
-
-    Cleaning file for LaTeX typesetting
-    """
+    """Clean file for LaTeX typesetting."""
     # open your csv and read as a text string
-    with open(file, 'r') as csv_user_data_file:
+    with open(file, "r") as csv_user_data_file:
         csv_text = csv_user_data_file.read()
         # substitute
-        new_csv_text = re.sub("&#39;", "\'",
-                              re.sub('\"', "",
-                                     re.sub(r"\\([\w]+)", r"\1", csv_text)))
+        new_csv_text = re.sub(
+            "&#39;", "'", re.sub('"', "", re.sub(r"\\([\w]+)", r"\1", csv_text))
+        )
+
         # open file and save
     with open(file, "w") as csv_user_data_file:
         csv_user_data_file.write(new_csv_text)
 
 
 if __name__ == "__main__":
-    user_id_data = {66024: 'tex', 690: 'physics'}
-    for user_id in user_id_data:
-        create_user_data_files(
-            request_for_questions(user_id, user_id_data[user_id]),
-            user_id_data[user_id]
-            )
+
+    se_api_methods = ["questions", "posts", "answers"]
+
+    user_id_data = {
+        "physics": 690,
+        "mathematics": 528313,
+        "tex": 66024,
+        "chemistry": 12641,
+        "stackoverflow": 4908648,
+    }
+
+    with open("user_tex_questions.json", "r") as info_dict_file:
+        info_dict = json.load(info_dict_file)
+
+    print(info_dict["items"])
+
+    # create_user_info_data_file(info_dict, "user_tex_questions.csv")
